20407c2fd9cc1f0007f2f719b9a3b9182ba5ce7a
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-04 20:29:56 -0700

    Added the form to create a new food item to the top of index

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -1,5 +1,33 @@
 class FoodItemsController < ApplicationController
   def index
     @food_items = FoodItem.all
+    @food_item = FoodItem.new
+  end
+
+  #TODO: not needed?
+  def show
+    food_item_id = params[:id]
+    @food_item = food_item.find_by(id: food_item_id)
+    if @food_item.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def create
+    @food_item = FoodItem.new(food_item_params) 
+    if @food_item.save 
+      redirect_to food_items_path
+      return
+    else 
+      render :index 
+      return
+    end
+  end
+
+  private
+
+  def food_item_params
+    return params.require(:food_item).permit(:name, :category, :units)
   end
 end
