8ac1db3cbb8e57691ad7d51f675cd96d6d9ff02b
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-04 19:06:22 -0700

    Added food items index route and view

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -1,2 +1,5 @@
 class FoodItemsController < ApplicationController
+  def index
+    @food_items = FoodItem.all
+  end
 end
--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -2,4 +2,5 @@ class FoodItem < ApplicationRecord
   has_many :ingredients
 
   validates :name, presence: true, uniqueness: true
+  default_scope { order(name: :desc) }
 end
