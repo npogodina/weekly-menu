85fa9e990c52af3990dc17a98f994fa18faa82b0
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-05 11:36:56 -0700

    Added new and create route for dishes + view

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,4 +2,33 @@ class DishesController < ApplicationController
   def index
     @dishes = Dish.order(:name)
   end
+
+  def show
+    @dish = Dish.find_by(id: params[:id])
+    if @dish.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def new
+    @dish = Dish.new
+  end
+
+  def create
+    @dish = Dish.new(dish_params) 
+    if @dish.save 
+      redirect_to dish_path(@dish)
+      return
+    else 
+      render :new 
+      return
+    end
+  end
+
+  private
+
+  def dish_params
+    return params.require(:dish).permit(:name, :servings, :meal, :recipe)
+  end
 end
